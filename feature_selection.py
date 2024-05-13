4bf240459d7e6305da306b9d3098a86929c067c0
Claire Tosolini <ctosolini@student.unimelb.edu.au>
2024-05-13 16:27:18 +1000

    various feature selection methods

--- feature_selection.py
+++ feature_selection.py
@@ -10,69 +10,179 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from data_preprocessing import *
 from sklearn.decomposition import PCA
-from sklearn.feature_selection import mutual_info_classif, SelectKBest, f_classif, chi2
+from sklearn.feature_selection import mutual_info_classif, SelectKBest, f_classif, chi2, SequentialFeatureSelector
+from sklearn.model_selection import cross_val_score
+from collections import OrderedDict
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+THRESHOLD_CORR = 0.045
+N_COMPONENTS = 180
+RANDOM_STATE = 123
+THRESHOLD_DT = 0.01
 
 # dimensionality reduction through selecting features with linear correlation above threshold. simplest and most intuitive but: correlation does not imply causality!
+# static, done before input into learner 
 def lin_correlation(train_df):
-    #sns.heatmap(train_df.corr())
-    #plt.show()
 
+    # Calculate linear correlation of each feature to the class label, in descending order
     correlations = pd.DataFrame(train_df.corr()['imdb_score_binned'].sort_values(ascending=False))
     correlations.columns = ['Correlation with imdb_score_binned']
     print(correlations)
 
-    print(correlations[correlations['Correlation with imdb_score_binned'] > 0.15]) # greater than a threshold 
+    # Plot all correlations to choose a threshold value: just before the graph flattens out
+    correlations.plot()
+    plt.xlabel('Column Index')
+    plt.ylabel('Correlation')
+    plt.title('Correlation of each feature with imdb_score_binned')
+    plt.show()
 
-# dimensionality reduction through univariate feature selection: chi2, fscore, MI 
-    # https://scikit-learn.org/stable/modules/feature_selection.html
+    # Features which have a correlation with the class label column greater than the chosen threshold 
+    df_selected = correlations[correlations['Correlation with imdb_score_binned'] > THRESHOLD_CORR]
+    print(df_selected, df_selected.count())
 
-# for a given K value, calculate 
-def filtering(train_df, k):
-
-    train_df_labels = train_df['imdb_score_binned']
-    train_df_features = train_df.drop('imdb_score_binned', axis=1)
-
-    train_df_kbest_fscore = SelectKBest(f_classif, k=k).fit_transform(train_df_features, train_df_labels)
-    print('k=', k, ', ')
-    train_df_kbest_chi2 = SelectKBest(chi2, k=k).fit_transform(train_df_features, train_df_labels)
-    train_df_kbest_mi = SelectKBest(mutual_info_classif, k=k).fit_transform(train_df_features, train_df_labels)
+    return df_selected 
 
 
 # dimensionality reduction according to explained variance 
+# static
 def pca(train_df):
 
+    # Remove class label column
     train_df_features = train_df.drop('imdb_score_binned', axis=1)
+    train_df_labels = train_df['imdb_score_binned']
 
+    # Fit PCA model to the training data and calculate the cumulative variance ratio i.e., how much variance is explained by n-1 principal components. 
     pca = PCA().fit(train_df_features)
     cumulative_variance_ratio = np.cumsum(pca.explained_variance_ratio_)
+
+    # Plot to observe number of components such that maximal variance is explained, but not maximising computational time. 
     plt.plot(cumulative_variance_ratio)
 
     # Get rid of auto axis scaling 
     ax = plt.gca()
-    ax.ticklabel_format(useOffset=False)   
+    ax.ticklabel_format(useOffset=False) 
 
     plt.title('Number of princical components needed for percentage of variance explained')
     plt.xlabel('Number of Components')
     plt.ylabel('Cumulative Explained Variance Ratio')
     plt.show()
 
-    pca = PCA(n_components=180) # n_components chosen from above graph 
+    # Choose n_components based on graph
+    pca = PCA(n_components=N_COMPONENTS) 
     train_df_reduced = pd.DataFrame(pca.fit_transform(train_df_features))
     return train_df_reduced
 
 
-# forward SFS (wrapper)
+# forward Sequential Feature Selection (wrapper)
+# dynamic: tuning requires input from learner 
+# Find n features such that it maximises cross-validated score 
+# For the range of n values given, find the n which maximises classifier accuracy 
+
+def forward_sfs(features, labels, n_min, n_max, n_step, classifier):
+    max_acc_n = 0
+    max_acc = 0
+
+    for i in range(n_min, n_max, n_step):
+        # calculate sfs
+        sfs = SequentialFeatureSelector(classifier, i)
+
+        # calculate accuracy 
+        #sfs.fit_transform(features, labels)
+        score = cross_val_score(sfs, features, labels, cv=5)
+        if (score.mean() > max_acc):
+            max_acc = score.mean()
+            max_acc_n = i
+
+    return max_acc_n, max_acc
+
+
     
 # embedded: decision tree
+def selection_dt(features, labels):
+    clf = DecisionTreeClassifier(random_state=RANDOM_STATE)
+    clf.fit(features, labels)
+
+    feature_names = features.columns
+    feature_importances = pd.DataFrame(feature_names)
+
+    # Get importances of all features according to classifier
+    feature_importances['feature importances'] = pd.DataFrame(clf.feature_importances_)
+    # Sort in descending order 
+    feature_importances = feature_importances.sort_values(by='feature importances', ascending=False).reset_index()
+    
+
+    feature_importances.columns = ['Feature Index', 'Feature Name', 'Feature Importance']
+
+    plt.plot(feature_importances['Feature Name'], feature_importances['Feature Importance'])
+    plt.xticks(np.arange(0, len(feature_importances)+1, 10), rotation=90)
+    plt.title("Features and their corresponding Importance according to Decision Tree Classifier")
+    plt.xlabel("Features (for visibility, only some names are shown)")
+    plt.ylabel("Feature Importance")
+    plt.show()
+
+    # Determine threshold through observing plot: return features with importance above this threshold
+    features_selected = feature_importances[feature_importances['Feature Importance']>THRESHOLD_DT]
+    print(features_selected)
+
+
+
+
     
 # filtering: based on PMI, MI, Chi-Square  
+    # dimensionality reduction through univariate feature selection: chi2, fscore, MI 
+    # https://scikit-learn.org/stable/modules/feature_selection.html
+    # adapted from https://scikit-learn.org/stable/auto_examples/ensemble/plot_ensemble_oob.html
 
+# For a given K value, find the K best features according to different metrics
+def filtering(features, labels, min_k, max_k, k_step, classifier):
+
+    ensemble_methods = [
+        (
+            "SelectKBest, metric = f score",
+            SelectKBest(f_classif),
+        ),
+        (
+            "SelectKBest, metric = chi squared",
+            SelectKBest(chi2),
+        ),
+        (
+            "SelectKBest, metric = mutual information",
+            SelectKBest(mutual_info_classif),
+        )
+    ]
+
+    accuracy = OrderedDict((label, []) for label, _ in ensemble_methods)
+    
+    for label, kbest in ensemble_methods:
+        for i in range(min_k, max_k + 1, k_step):
+            kbest.set_params(k=i)
+            res = kbest.fit_transform(features, labels)
+
+            acc = cross_val_score(classifier, res, labels, cv=5).mean()
+            #print("accuracy =", acc)
+            accuracy[label].append((i, acc))
+
+    for label, acc in accuracy.items():
+        xs, ys = zip(*acc)
+        plt.plot(xs, ys, label=label)
+
+    plt.xlim(min_k, max_k)
+    plt.title("Number of k-best features versus accuracy of classifier")
+    plt.xlabel("n features")
+    plt.ylabel("classifier accuracy")
+    plt.legend(loc="upper right")
+    plt.show()
 
 def main():
     
     train_df_minmax, test_df_minmax, train_df_std, test_df_std = preprocess()
-    lin_correlation(train_df_minmax)
+
+    #train_df_minmax_corr = lin_correlation(train_df_minmax)
     #train_df_minmax_pca = pca(train_df_minmax)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    #filtering(train_df_minmax.drop('imdb_score_binned', axis=1), train_df_minmax['imdb_score_binned'], 10, 300, 10, knn)
+    selection_dt(train_df_minmax.drop('imdb_score_binned', axis=1), train_df_minmax['imdb_score_binned'])
     
 
 
